c81638fe52f1f883cdcaee9f001cf07e0c1e4883
Mark Sims <markedwardsims@gmail.com>
2016-04-11 12:31:59 -0500

    require text plugin added, knockout added, high level components created

--- app/main.js
+++ app/main.js
@@ -1,7 +1,23 @@
 define([
-
-], function(){
+	'knockout'
+], function(ko){
 	
-	console.log('hello warrior!');
+	ko.components.register('Header', {
+		template: { require: 'text!app/components/header/header.html' }
+	});
+
+	ko.components.register('WarriorsList', {
+		template: { require: 'text!app/components/warriorsList/warriorsList.html' }
+	});
+
+	ko.components.register('Leaderboard', {
+		template: { require: 'text!app/components/leaderboard/leaderboard.html' }
+	});
+
+	ko.components.register('Matchup', {
+		template: { require: 'text!app/components/matchup/matchup.html' }
+	});
+
+	ko.applyBindings();
 
 });
