eba9979ebfc9d8a90795438b58a1be551499855c
Mark Sims <markedwardsims@gmail.com>
2016-04-11 21:11:55 -0500

    viewmodel created for warrior component, component helper created to register all components

--- app/main.js
+++ app/main.js
@@ -1,30 +1,31 @@
 define([
-	'knockout'
-], function(ko){
+	'knockout',
+	'helpers/componentHelper'
+], function(ko, componentHelper){
 	
-	ko.components.register('Header', {
-		template: { require: 'text!app/components/header/header.html' }
-	});
+	var componentConfig = {
+		'Header': {
+			template: { require: 'text!components/header/header.html' }
+		},
+		'WarriorsList': {
+			template: { require: 'text!components/warriorsList/warriorsList.html' }
+		},
+		'Leaderboard': {
+			template: { require: 'text!components/leaderboard/leaderboard.html' }
+		},
+		'Matchup': {
+			template: { require: 'text!components/matchup/matchup.html' }
+		},
+		'Warrior': {
+			template: { require: 'text!components/warrior/warrior.html' },
+			viewModel: { require: 'components/warrior/warrior' }
+		},
+		'WarriorDetail': {
+			template: { require: 'text!components/warriorDetail/warriorDetail.html' }
+		}
+	};
 
-	ko.components.register('WarriorsList', {
-		template: { require: 'text!app/components/warriorsList/warriorsList.html' }
-	});
-
-	ko.components.register('Leaderboard', {
-		template: { require: 'text!app/components/leaderboard/leaderboard.html' }
-	});
-
-	ko.components.register('Matchup', {
-		template: { require: 'text!app/components/matchup/matchup.html' }
-	});
-
-	ko.components.register('Warrior', {
-		template: { require: 'text!app/components/warrior/warrior.html' }
-	});
-
-	ko.components.register('WarriorDetail', {
-		template: { require: 'text!app/components/warriorDetail/warriorDetail.html' }
-	});
+	new componentHelper().registerComponents(componentConfig);
 
 	ko.applyBindings();
 
