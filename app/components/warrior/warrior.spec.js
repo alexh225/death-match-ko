9134e7646f82b9ab61ab7bbcef4b7e41d5c9c7e1
Mark Sims <markedwardsims@gmail.com>
2016-04-28 10:17:40 -0500

    adding karma to run unit tests, warrior.spec.js file created

---
+++ app/components/warrior/warrior.spec.js
@@ -0,0 +1,19 @@
+define([
+	'components/warrior/warrior'
+], function(Warrior){
+
+	describe('The Warrior component viewmodel', function(){
+
+		it('should default the size to small if no parameter is provided', function(){
+			var warrior = new Warrior();
+			expect(warrior.size()).toBe('warrior--small');
+		});
+
+		it('should set the size properly if the parameter is provided', function(){
+			var warrior = new Warrior({ size: 'foo' });
+			expect(warrior.size()).toBe('warrior--foo');
+		});
+
+	});
+
+});
--- requireConfig.js
+++ requireConfig.js
@@ -2,6 +2,7 @@ requirejs.config({
     paths: {
         'components': 'app/components',
         'helpers': 'app/helpers',
+        'node_modules': 'node_modules',
         'text': 'node_modules/text/text',
         'knockout': 'node_modules/knockout/build/output/knockout-latest'
     }
